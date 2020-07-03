f609236bf9d613cf73166ddf3e7d3e3d065e1c2a
visko93 <vskonicezny@gmail.com>
2020-07-03 09:56:59 -0300

    First commit, project functional without otimization

---
+++ app/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Popular from './components/Popular'
+import Battle from "./components/Battle";
+
+
+import './index.css';
+
+
+// Components
+// States
+// Lifecycle
+// UI
+
+class App extends React.Component {
+
+  render () {
+    return(
+      <div className='container'>
+        <Battle />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
