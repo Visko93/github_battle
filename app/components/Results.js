1aed40209b99290f4cd486fec085c65d00965b96
visko93 <vskonicezny@gmail.com>
2020-07-04 23:19:56 -0300

    Add tooltip component High Order Function

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,24 +4,39 @@ import PropTypes from "prop-types";
 
 import Loading from "./Loading";
 import Card from "./Card";
+import Tooltip from "./Tooltip";
 
 import { FaUser, FaCompass, FaSuitcase, FaUsers, FaUserFriends, FaCode, FaSpinner } from 'react-icons/fa';
+
 
 
 function ProfileList({ profile }) {
   return (
     <ul className='card-list'>
-    <li><FaUser color='rgb(239,115,115)' size={22} /> {profile.name}</li>
-    {profile.location && 
-      <li><FaCompass color='rgb(144,115,255)' size={22} /> {profile.location}</li>
-    }
-    {profile.company &&
-      <li><FaSuitcase color='#795548' size={22} /> {profile.company}</li>
-    }
-    <li><FaUsers color='rgb(129,195,245)' size={22} /> {profile.followers.toLocaleString()} followers</li>
-    <li><FaUserFriends color='rgb(64,183,95)' size={22} /> {profile.following.toLocaleString()} following</li>
-    <li><FaCode color='' size={22} /> {profile.public_repos.toLocaleString()} repositories</li>
-  </ul>
+      <li>
+        <FaUser color='rgb(239,115,115)' size={22} /> 
+          {profile.name}
+      </li>
+      {profile.location && 
+      <li>
+        <Tooltip text="User's Location">
+          <FaCompass color='rgb(144,115,255)' size={22} /> 
+          {profile.location}
+        </Tooltip>
+      </li>
+      }
+      {profile.company &&
+        <li>
+          <Tooltip text="User's Company">
+            <FaSuitcase color='#795548' size={22} /> 
+            {profile.company}
+          </Tooltip>
+        </li>
+      }
+      <li><FaUsers color='rgb(129,195,245)' size={22} /> {profile.followers.toLocaleString()} followers</li>
+      <li><FaUserFriends color='rgb(64,183,95)' size={22} /> {profile.following.toLocaleString()} following</li>
+      <li><FaCode color='' size={22} /> {profile.public_repos.toLocaleString()} repositories</li>
+    </ul>
   )
 }
 
@@ -112,4 +127,26 @@ Results.propTypes = {
   playerOne: PropTypes.string.isRequired,
   playerTwo: PropTypes.string.isRequired,
   onReset: PropTypes.func.isRequired,
+}
+
+const styles = {
+  container: {
+    position: 'relative',
+    display: 'flex'
+  },
+  tooltip: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '160px',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-80px',
+    borderRadius: '3px',
+    backgroundColor: '3px',
+    padding: '7px',
+    marginBottom: '5px',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: '14px',
+  }
 }
